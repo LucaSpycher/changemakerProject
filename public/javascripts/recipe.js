441f17c1dbe50e341a6e720a77340d705e5e9057
Robert Ezra Stern <robertstern@students.berkeley.net>
2018-05-23 11:23:07 -0700

    recipe-search page functionality

---
+++ public/javascripts/recipe.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+    $("input").keyup(function(){
+        if(event.code == "Enter" && this.value!=""){
+            $(".result").remove();
+            var searchterm = this.value.toLowerCase().replace(" ","+");;
+            $.ajax({
+                url: 'https://www.themealdb.com/api/json/v1/1/search.php?s='+searchterm,
+                success: function(result){
+                    console.log(result);
+                    for(var i=0; i<result.meals.length; i++){
+                        console.log(result[i]);
+                        $("body").append("<div class='result'>"+result.meals[i].strMeal+"</div>");
+                    }
+                },
+                error: function () {
+                    $("body").append("<div class='result error'>Error in searching</div>");
+                }
+            });
+        }
+    });
+});
