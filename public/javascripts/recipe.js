1cfaed75fe053e3b11ee26d2ab63ec695787c42d
Robert Ezra Stern <robertstern@students.berkeley.net>
2018-05-29 11:21:56 -0700

    Added link and image-centering functionality, addtl tag

--- public/javascripts/recipe.js
+++ public/javascripts/recipe.js
@@ -5,6 +5,7 @@ $(document).ready(function(){
             $.ajax({
                 url: 'https://www.themealdb.com/api/json/v1/1/search.php?s='+searchterm,
                 success: function(result){
+                    console.log(result);
                     $("#res").empty();
                     for(var i=0; i<result.meals.length; i++){
                         $("#res").append("<div class='result'><img src='"
@@ -13,17 +14,29 @@ $(document).ready(function(){
                             +result.meals[i].strMeal.toUpperCase()
                             +"</em><i>"
                             +result.meals[i].strArea
+                            +"</i> <i>"
+                            +removeTag(result.meals[i].strTags)
                             +"</i> Ingredients: "
                             +result.meals[i].strIngredient1+", "
                             +result.meals[i].strIngredient2+", "
                             +result.meals[i].strIngredient3
-                            +"...</span></div>");
+                            +"...</span><img class='linkimg' src='public/images/plus.svg' data-source='"
+                            +result.meals[i].strSource
+                            +"'></div>");
                         $("#res div:last-of-type").css("opacity",".05");
                     }
                     for(var i=0; i<result.meals.length; i++){
                         $("#res div.result:nth-child("+(i+1)+")").delay(100*i).animate({"opacity":1},200);
                     }
-                    $("#res img").click(function(){
+                    $("div#res div.result img.linkimg").click(function(){
+                        var win = window.open($(this).data("source"));
+                        if (win) {
+                            win.focus();
+                        } else {
+                            alert('Please allow popups for this website');
+                        }
+                    });
+                    $("#res img:not(img.linkimg)").click(function(){
                         if(!$(this).is(".focus")){
                             $("#res img").removeClass("focus");
                             $(this).toggleClass('focus');
@@ -31,6 +44,10 @@ $(document).ready(function(){
                                 "left":($(window).width()-$(this).width())/2+"px"});
                             $("body").append("<div class='shade'></div>");
                             $("nav, input[type=text], #res span, #res img:not(img.focus)").css("filter","blur(6px)");
+                            $(window).resize(function(){
+                                $("#res img.focus").css({"top":($(window).height()-$("#res img.focus").height())/2+"px",
+                                    "left":($(window).width()-$("#res img.focus").width())/2+"px"});
+                            });
                         }else{
                             $("#res img").removeClass("focus");
                             $(".shade").remove();
@@ -47,3 +64,13 @@ $(document).ready(function(){
         }
     });
 });
+
+function removeTag(str){
+    if(str==null){
+        return "No Tags";
+    }if(str.indexOf(",")==-1){
+        return str;
+    }else{
+        return str.substring(0,str.indexOf(","));
+    }
+}
