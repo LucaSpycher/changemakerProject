812843c4c0d0ce9cbaad696b79405afd270fe093
Robert Ezra Stern <robertstern@students.berkeley.net>
2018-06-05 11:10:03 -0700

    Implementing user

--- public/javascripts/changeMaker.js
+++ public/javascripts/changeMaker.js
@@ -9,11 +9,22 @@ function setupNav(n){
             $("nav a:last-child").addClass("current");
         }
     }
-    $("nav").append("<img src='public/images/user.png' id='user'>").append("<div id='login'></div>");
+    $("nav").append("<img src='public/images/user.png' id='user'>").append("<div id='login'><table><tr><td>New User</td><td>Existing User</td></tr></table></div>");
     $("#login").hide();
     $("#user").click(function(){
         $("#login").fadeToggle(200);
-    })
+    });
+    $("#login td:first-child").click(function(){
+        console.log("new user");
+    });
+    $("#login td:nth-child(2)").click(function(){
+        $(this).empty().append("<input id='numUser' type='number'>");
+        $("#numUser").keyup(function(){
+            if(event.code == "Enter" && this.value!=""){
+                console.log("user #"+$("#numUser").val());
+            }
+        });
+    });
 }
 
 function setupMealPlanTable() {
