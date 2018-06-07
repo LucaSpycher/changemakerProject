29f85d91cbe52a3b13e75ef869ee2fe7ab9be655
Luca Spycher <lucaspycher@students.berkeley.net>
2018-06-07 11:38:19 -0700

    updates to mealplan

--- public/javascripts/mealplan.js
+++ public/javascripts/mealplan.js
@@ -181,7 +181,5 @@ function displayMealsInDays() {
     $('.mealName').on('click', function () {
         $(this).toggleClass('selectedRemove');
     });
-    //saveMeal();
-
-    console.log(imgArray);
+    saveMeal();
 }
